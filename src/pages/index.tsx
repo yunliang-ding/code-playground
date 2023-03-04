95292762321505b37d78f04055a2428acccbf18f
lighthouse-8G <lighthouse@8g.com>
2023-03-04 14:26:10 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,59 @@
-import RedirectFristMenu from '@/components/redirect-first-menu';
+import { useState, memo } from 'react';
+import { CodeEditor, getTools } from 'react-core-form-designer';
+import './index.less';
 
-export default RedirectFristMenu();
+const { encode, decode } = getTools();
+
+const defualtCode = `export default () => {
+  return 'hello world';
+}`;
+
+export default () => {
+  const params: any = new URLSearchParams(location.hash.split('?')[1]);
+  const [code, setCode] = useState(
+    params.get('code') ? decode(params.get('code')) : defualtCode,
+  );
+  return (
+    <div className="playground">
+      <div className="playground-left">
+        <CacheEditor
+          code={code}
+          setCode={(code) => {
+            setCode(code);
+            window.parent.postMessage(
+              {
+                type: 'playground',
+                content: code,
+              },
+              '*',
+            );
+          }}
+        />
+      </div>
+      <div className="playground-right">
+        <iframe
+          key={code}
+          src={`/#/preview?code=${encode(code)}`}
+        />
+      </div>
+    </div>
+  );
+};
+
+const CacheEditor = memo(
+  ({ code, setCode }: any) => {
+    return (
+      <CodeEditor
+        style={{
+          width: '50vw',
+          height: '100vh',
+        }}
+        value={code}
+        onSave={setCode}
+      />
+    );
+  },
+  () => {
+    return true;
+  },
+);
--- src/services/common.tsx
+++
@@ -1,9 +0,0 @@
-import { request } from 'ice';
-
-export const outLogin = () => {
-  return request.post('/proxy/unification/logout');
-};
-
-export const userInfo = () => {
-  return request.post('/proxy/user/userinfo');
-};
