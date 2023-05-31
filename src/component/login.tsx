c80011617a2de15c2083e4440a27f2441d360031
lighthouse-8G <lighthouse@8g.com>
2023-05-31 10:40:06 +0800

    fixed

--- src/component/login.tsx
+++ src/component/login.tsx
@@ -2,11 +2,18 @@ import { setUser } from '@/util';
 import { Input, Modal } from 'antd';
 import { useEffect, useRef } from 'react';
 import { Button } from 'react-core-form';
-import { isEmpty } from 'react-core-form-tools'
+import { isEmpty } from 'react-core-form-tools';
 import './index.less';
 
 export default () => {
   const inputRef: any = useRef({});
+  const submit = async () => {
+    await new Promise((res) => setTimeout(res, 500));
+    if (!isEmpty(inputRef.current.input.value)) {
+      setUser(inputRef.current.input.value);
+      location.reload();
+    }
+  };
   useEffect(() => {
     Modal.info({
       className: 'login-modal',
@@ -26,18 +33,12 @@ export default () => {
             gap: 10,
           }}
         >
-          <Input placeholder="请输入昵称" ref={inputRef} />
-          <Button
-            spin
-            type='primary'
-            onClick={async () => {
-              await new Promise((res) => setTimeout(res, 500));
-              if (!isEmpty(inputRef.current.input.value)) {
-                setUser(inputRef.current.input.value);
-                location.reload();
-              }
-            }}
-          >
+          <Input
+            placeholder="请输入昵称"
+            ref={inputRef}
+            onPressEnter={submit}
+          />
+          <Button spin type="primary" onClick={submit}>
             提交
           </Button>
         </div>
